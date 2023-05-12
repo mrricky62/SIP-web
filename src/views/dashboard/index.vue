944b2bf51e39c955de42d7631b11e4c85d775847
lazlanrafar <lazlanrafar@gmail.com>
2023-05-12 12:00:45 +0700

    slicing dashboard

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,64 +1,33 @@
 <template>
   <layout-app>
     <div class="card border-0">
-      <div class="card-body">
-        <div class="d-flex flex-column flex-sm-row gap-2 gap-md-3">
-          <CardHome type="pemasukan" :value="reports.historyIn" />
-          <CardHome type="pengeluaran" :value="reports.historyOut" />
-        </div>
-
-        <br />
-        <div class="p-3">
-          <p class="fw-semibold">Riwayat</p>
-          <v-data-table :headers="headers" :items="reports.history">
-            <template v-slot:[`item.date`]="{ item }">
-              {{ moment(item.date).format("YYYY-MM-DD") }}
-            </template>
-          </v-data-table>
-        </div>
+      <div class="card-body p-4 p-md-5">
+        <h4>Welcome 👋</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem suscipit repudiandae soluta fuga quam facilis quisquam
+          odit alias, modi facere et excepturi sapiente ipsam. Nisi impedit
+          earum incidunt laboriosam ipsam.
+        </p>
       </div>
     </div>
   </layout-app>
 </template>
 
 <script>
-import moment from "moment";
 import LayoutApp from "../../layouts/layout-app.vue";
 
 export default {
   name: "Home",
   components: {
     LayoutApp,
-    CardHome: () => import("@/components/molecules/card-home.vue"),
   },
   data() {
-    return {
-      moment,
-      headers: [
-        { text: "Date", value: "date" },
-        { text: "Barang", value: "barang.name" },
-        { text: "Barang Description", value: "barang.description" },
-        { text: "Description", value: "description" },
-        { text: "Quantity Sebelum", value: "qty_before" },
-        { text: "Quantity Pengubah", value: "qty_change" },
-        { text: "Quantity Sesudah", value: "qty_after" },
-      ],
-    };
+    return {};
   },
   computed: {
     isLoading() {
       return this.$store.state.dashboard.isLoading;
-    },
-    reports() {
-      return this.$store.state.dashboard.reports;
-    },
-    optionsTable: {
-      get() {
-        return this.$store.state.dashboard.optionsTable;
-      },
-      set(value) {
-        this.$store.commit("SET_OPTIONS_TABLE_DASHBOARD", value);
-      },
     },
   },
   mounted() {
